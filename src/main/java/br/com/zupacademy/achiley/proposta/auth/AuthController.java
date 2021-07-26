4004e96f2f6d312d6c8707fbc502cead7467056e
achiley.pinto <achileyd@gmail.com>
2021-07-26 18:03:39 -0300

    Melhorando a visibilidade com Health Check

---
+++ src/main/java/br/com/zupacademy/achiley/proposta/auth/AuthController.java
@@ -0,0 +1,39 @@
+package br.com.zupacademy.achiley.proposta.auth;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zupacademy.achiley.proposta.config.security.TokenDto;
+import br.com.zupacademy.achiley.proposta.config.security.TokenService;
+
+@RestController
+public class AuthController {
+	@Autowired
+	private AuthenticationManager authManager;
+	
+	@Autowired
+	private TokenService tokenService;
+
+	@PostMapping("/auth")
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
+		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+		
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin);
+			String token = tokenService.gerarToken(authentication);
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+}
